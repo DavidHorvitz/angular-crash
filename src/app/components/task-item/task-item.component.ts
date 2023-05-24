88881a581ca885e512669d7f3eeb8790ea882c2d
DavidHorvitz <2205dzh@gmail.com>
2023-05-25 00:31:31 +0300

    I started working with services

--- src/app/components/task-item/task-item.component.ts
+++ src/app/components/task-item/task-item.component.ts
@@ -1,5 +1,7 @@
 import { Component, Input } from '@angular/core';
 import { Task } from 'src/app/Task';
+import { faCoffee } from '@fortawesome/free-solid-svg-icons';
+
 
 @Component({
   selector: 'app-task-item',
@@ -7,7 +9,8 @@ import { Task } from 'src/app/Task';
   styleUrls: ['./task-item.component.css']
 })
 export class TaskItemComponent {
-  @Input() task!: Task
+  @Input() task!: Task;
+  faTimes = faCoffee;
 
 
 }
